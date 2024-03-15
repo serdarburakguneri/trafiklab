6317599606af140029a8e5556f9eba36af50649b
Serdar Burak Guneri <serdar@vidispine.com>
2024-03-15 14:30:28 +0100

    refactoring: removed unnecessary map execution

--- src/main/java/com/sbg/trafiklab/service/JourneyService.java
+++ src/main/java/com/sbg/trafiklab/service/JourneyService.java
@@ -4,7 +4,6 @@ import com.sbg.trafiklab.dto.JourneyDTO;
 import com.sbg.trafiklab.dto.StopPointDTO;
 import com.sbg.trafiklab.repository.LineRepository;
 import com.sbg.trafiklab.repository.StopPointRepository;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -24,13 +23,15 @@ public class JourneyService {
 
     public Flux<JourneyDTO> findAll(int limit) {
 
+        // Seeing we can't have many-to-many relationships in R2DBC,
+        // we have to fetch all lines and then fetch all stop points to be able to aggregate them into a JourneyDTO
+
         return lineRepository.findAll()
-                .map(line -> new JourneyDTO(line.getLineNumber(), List.of()))
-                .flatMap(journey -> stopPointRepository.findByLineNumber(journey.lineNumber())
+                .flatMap(line -> stopPointRepository.findByLineNumber(line.getLineNumber())
                         .map(stopPoint ->
                                 new StopPointDTO(stopPoint.getStopPointNumber(), stopPoint.getStopPointName()))
                         .collectList()
-                        .map(stopPoints -> new JourneyDTO(journey.lineNumber(), stopPoints)))
+                        .map(stopPoints -> new JourneyDTO(line.getLineNumber(), stopPoints)))
                 .sort((journey1, journey2) -> Integer.compare(journey2.stops().size(),
                         journey1.stops().size()))
                 .take(limit);
